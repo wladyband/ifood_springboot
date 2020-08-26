f91d40bf966c72454380a2219ab295cc300803ac
Wladimir Bandeira <tecnico100@gmail.com>
2020-08-26 14:45:03 -0300

    criação inicial da aplicação

---
+++ wladifood/src/main/java/com/wladi/wladifood/WladifoodApplication.java
@@ -0,0 +1,13 @@
+package com.wladi.wladifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WladifoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WladifoodApplication.class, args);
+	}
+
+}
