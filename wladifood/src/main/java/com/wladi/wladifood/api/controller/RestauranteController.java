ff5e68053ca796cd8bf6aca1fc78ee455f0f63fe
Wladimir Bandeira <tecnico100@gmail.com>
2020-09-09 15:26:01 -0300

    implementação da atualização partial do restaurante

--- wladifood/src/main/java/com/wladi/wladifood/api/controller/RestauranteController.java
+++ wladifood/src/main/java/com/wladi/wladifood/api/controller/RestauranteController.java
@@ -1,12 +1,17 @@
 package com.wladi.wladifood.api.controller;
 
+
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wladi.wladifood.domain.exception.EntidadeNaoEncontradaException;
 import com.wladi.wladifood.domain.model.Restaurante;
 import com.wladi.wladifood.domain.repository.RestauranteRepository;
@@ -79,5 +85,36 @@ public class RestauranteController {
 					.body(e.getMessage());
 		}
 	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if (restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		merge(campos, restauranteAtual);
+		
+		return atualizar(restauranteId, restauranteAtual);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+//			System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+		});
+	}
+	
 	
 }
